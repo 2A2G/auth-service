e584a682ff7eb11a6424418029a0a0bc08c54110
Aldair Gutierrez <aldairgguer@gmail.com>
2025-02-22 19:43:33 -0500

    Insert table User

---
+++ src/main/java/com/authservice/auth_service/entity/user/User.java
@@ -0,0 +1,23 @@
+package com.authservice.auth_service.entity.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.IdGeneratorType;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
