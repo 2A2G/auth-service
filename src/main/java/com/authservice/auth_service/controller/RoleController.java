d8da3a00157b19027614ccbf31982ce956eacf89
Aldair Gutierrez <aldairgguer@gmail.com>
2025-02-24 23:48:34 -0500

    successful module role

--- src/main/java/com/authservice/auth_service/controller/RoleController.java
+++ src/main/java/com/authservice/auth_service/controller/RoleController.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api-serve/role")
 public class RoleController {
@@ -24,6 +27,17 @@ public class RoleController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating role");
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Role>> getRoles() {
+        try {
+            List<Role> roles = roleService.view();
+            return ResponseEntity.status(HttpStatus.OK).body(roles);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Collections.emptyList());
         }
     }
 }
